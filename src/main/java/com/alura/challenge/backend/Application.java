cf662859d4a27bccb2fda983f87e8566bde5d2b3
Euller Lisowski <eullersl@gmail.com>
2024-05-29 18:29:35 -0300

    cleanup: Ajusta nome do package principal

--- src/main/java/com/alura/challenge/backend/Application.java
+++ src/main/java/com/alura/challenge/backend/Application.java
@@ -1,4 +1,4 @@
-package com.alura.challenge.backend_7;
+package com.alura.challenge.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
