4aadd3bb4d520179f4ae72cd4e7b0d77f2f5429c
Euller Lisowski <eullersl@gmail.com>
2024-05-28 23:14:45 -0300

    :tada: init: Inicializa projeto base com pacotes gerados pelo spring initilizr

---
+++ src/test/java/com/alura/challenge/backend_7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.backend_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
