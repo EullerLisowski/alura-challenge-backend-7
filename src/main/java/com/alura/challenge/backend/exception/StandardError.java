3e4a6ef58c107ad5a5b933635173be3bd869db57
Euller Lisowski <eullersl@gmail.com>
2024-06-16 15:31:09 -0300

    feat: Adiciona classe para padronizar erros

---
+++ src/main/java/com/alura/challenge/backend/exception/StandardError.java
@@ -0,0 +1,50 @@
+package com.alura.challenge.backend.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StandardError implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -8346768838153267263L;
+
+    private Long timeStamp;
+    private Integer status;
+    private String error;
+    private List<DataValidationError> errors;
+    private String message;
+    private String path;
+    private Map<String, Object> map;
+
+    public StandardError(Long timeStamp, Integer status, String error, String message, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+
+    public StandardError(Long timeStamp, Integer status, String error, String path) {
+        this.timeStamp = timeStamp;
+        this.status = status;
+        this.error = error;
+        this.path = path;
+    }
+
+    public StandardError(Integer status, String message, String path) {
+        this.timeStamp = System.currentTimeMillis();
+        this.status = status;
+        this.message = message;
+    }
+}
