18d1ea2b4599549dbed9adff0451341fed233b70
Euller Lisowski <eullersl@gmail.com>
2024-06-18 19:39:57 -0300

    test: Adiciona testes para StandardError

---
+++ src/test/java/com/alura/challenge/backend/exception/StandardErrorTest.java
@@ -0,0 +1,44 @@
+package com.alura.challenge.backend.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.FieldError;
+
+@SpringBootTest(classes = StandardErrorTest.class)
+class StandardErrorTest {
+
+    @Test
+    void givenNoArgsConstructor_whenCalled_thenObjectCreated() {
+
+        StandardError error = new StandardError();
+
+        assertNull(error.getMessage());
+        assertNull(error.getPath());
+        assertNull(error.getStatus());
+        assertNull(error.getTimeStamp());
+    }
+
+    @Test
+    void givenAllArgsConstructor_whenCalled_thenObjectCreated() {
+        Long TIME_STAMP = System.currentTimeMillis();
+        Integer STATUS = 404;
+        FieldError FIELD_ERROR = new FieldError("objectName", "field", "message");
+        DataValidationError DATA_VALIDATION_ERROR = new DataValidationError(FIELD_ERROR);
+        List<DataValidationError> ERRORS = List.of(DATA_VALIDATION_ERROR);
+        String MESSAGE = "Test message";
+        String PATH = "/test-uri";
+
+        StandardError error = new StandardError(TIME_STAMP, STATUS, ERRORS, MESSAGE, PATH);
+
+        assertEquals(TIME_STAMP, error.getTimeStamp());
+        assertEquals(STATUS, error.getStatus());
+        assertEquals(MESSAGE, error.getMessage());
+        assertEquals(PATH, error.getPath());
+        assertEquals(ERRORS, error.getErrors());
+    }
+}
