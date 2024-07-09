3a95a6e3ce4aff810ee5568ced9808a950704b83
Euller Lisowski <eullersl@gmail.com>
2024-07-09 14:44:42 -0300

    feat: Altera método update da DepoimentoService para fazer a atualizacao respeitando as datas

--- src/main/java/com/alura/challenge/backend/repositories/DepoimentoRepository.java
+++ src/main/java/com/alura/challenge/backend/repositories/DepoimentoRepository.java
@@ -1,10 +1,18 @@
 package com.alura.challenge.backend.repositories;
 
 import com.alura.challenge.backend.domain.entities.Depoimento;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface DepoimentoRepository extends JpaRepository<Depoimento, Long> {
 
+    @Modifying
+    @Transactional
+    @Query("update Depoimento d set d.nome = ?2, d.depoimento =?3, d.foto = ?4, d.dataAtualizacao = now() where d.id = ?1")
+    Integer setDepoimentoInfoById(Long id, String nome, String depoimento, String foto);
 }
