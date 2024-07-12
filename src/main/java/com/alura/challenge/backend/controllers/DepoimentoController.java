b11e239d0d5ca1f818bfd5e0773d68edb8f9111c
Euller Lisowski <eullersl@gmail.com>
2024-07-11 23:10:38 -0300

    cleanup: Adiciona somente imports necessários e ajusta path do request mapping

--- src/main/java/com/alura/challenge/backend/controllers/DepoimentoController.java
+++ src/main/java/com/alura/challenge/backend/controllers/DepoimentoController.java
@@ -1,5 +1,19 @@
 package com.alura.challenge.backend.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.alura.challenge.backend.domain.dtos.DepoimentoDto;
 import com.alura.challenge.backend.domain.dtos.DepoimentoInsertDto;
 import com.alura.challenge.backend.domain.dtos.DepoimentoUpdateDto;
@@ -10,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping("depoimentos")
+@RequestMapping
 public class DepoimentoController {
 
     private final DepoimentoService service;
@@ -19,7 +33,7 @@ public class DepoimentoController {
         this.service = service;
     }
 
-    @PostMapping
+    @PostMapping("/depoimentos")
     public ResponseEntity<DepoimentoDto> create(@RequestBody @Validated DepoimentoInsertDto dto) {
         var depoimento = service.create(dto);
         var location = ServletUriComponentsBuilder
@@ -31,23 +45,23 @@ public class DepoimentoController {
         return ResponseEntity.created(location).body(depoimento);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/depoimentos/{id}")
     public ResponseEntity<DepoimentoDto> findById(@PathVariable Long id) {
         var depoimento = service.findById(id);
         return ResponseEntity.ok().body(depoimento);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/depoimentos/{id}")
     public ResponseEntity<DepoimentoDto> update(@PathVariable Long id, @RequestBody DepoimentoUpdateDto dto) {
         var depoimento = service.update(id, dto);
 
         return ResponseEntity.ok().body(depoimento);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/depoimentos/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
-        
+
         return ResponseEntity.noContent().build();
     }
 }
